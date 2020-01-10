144ef9f7e42c6ace78492ee7380659534fbad4ff
gabrielaaa99 <avi19002@byui.edu>
2020-01-10 16:09:59 -0400

    update

--- js/lastupdate.js
+++ js/lastupdate.js
@@ -1,2 +1,32 @@
-var x = document.lastModified; 
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + "," + monthName + " " + d.getDate() + "," + d.getFullYear();
+
+var x = document.lastModified;
 document.getElementById("lastUpdate").innerHTML = x;
