aa272babb09f810fca2f49acb4c7675f2dad913a
gabrielaaa99 <avi19002@byui.edu>
2020-02-06 22:18:41 -0400

    update

---
+++ lesson5/js/fridayAlert.js
@@ -0,0 +1,16 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let dayName = daynames[d.getDay()];
+
+var day= d.getDay();
+if (day === "Thursday") {
+    alert (It is Friday);
+}
