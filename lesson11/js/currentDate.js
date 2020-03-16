5ef737d43ddbcc6d0b6937b000775212954823b6
gabrielaaa99 <avi19002@byui.edu>
2020-03-16 15:39:34 -0400

    update

---
+++ lesson11/js/currentDate.js
@@ -0,0 +1,33 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " +  d.getDate() + " " + monthName + " " + d.getFullYear(); 
+
+document.getElementById("currentDate").innerHTML = fulldate;
+
+
---
+++ lesson11/js/forecast.js
@@ -0,0 +1,60 @@
+const wURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e5471d9c45ee420f0d1bffd165f48ae8&units=imperial";
+
+fetch(wURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+        let currentW = document.getElementById('current');
+        const getWeather = jsObject.weather[0].main;
+        currentW.textContent = getWeather;
+
+        let temperat = document.querySelector("#temperature");
+        temperat.textContent = jsObject.main.data;
+
+        let humidity = document.querySelector("#humidity");
+        humidity.textContent = jsObject.main.humidity;
+
+        let windSpeed = document.querySelector("#wspeed");
+        windSpeed.textContent = jsObject.wind.speed;
+
+        let windChill = document.getElementById('#windchill');
+        let func = 35.74 + (0.6215 * temperature) - (35.75 * (Math.pow(wspeed, 0.16))) + (0.4275 * temperature * (Math.pow(wspeed, 0.16)));
+
+        if (temperature <= 50 && wspeed >= 3) {
+            windChill.textContent = Math.round(func);
+        } else {
+            windChill.textContent = "N/A";
+        }
+    }
+,);
+
+const fURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=e5471d9c45ee420f0d1bffd165f48ae8&units=imperial";
+
+fetch(fURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+    var date = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+        const forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+        for (let i = 0; i < forecast.length; i++) {
+    document.getElementById(`data${i + 1}`).textContent = forecast[i].main.temp.toFixed(0) + "\xB0";
+    document.getElementById(`label${i + 1}`).textContent = weekday[(date.getDay() + i + 1) % 7];
+
+    document.getElementById(`pic${i + 1}`).setAttribute('src', "https://openweathermap.org/img/wn/" + forecast[i].weather[0].icon + "@2x.png");
+    document.getElementById(`pic${i + 1}`).setAttribute('alt', forecast[i].weather[0].description);
+
+
+}
+    
+});
